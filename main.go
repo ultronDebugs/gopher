2b6233e922b054fdb2c3646ff066375d91dca989
ultronDebugs <muftahuddeenabba@gmail.com>
2026-01-11 18:04:30 +0100

    configured router

--- main.go
+++ main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the home page!"))
+	})
+
 	fmt.Println("Hello, World!")
 	server := &http.Server{Addr: ":8080",
-		Handler: http.HandlerFunc(basicHandler)}
+		Handler: router}
 
 	err := server.ListenAndServe()
 
